0f5c78db331e921328ebf97d70886ac5b04636a8
Donell <donell.wambua@student.moringaschool.com>
2023-08-26 23:35:39 +0300

    Completed assignment

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount_percent):
+        self.discount_percent = discount_percent
+
+    def calculate_total_price(self, price):
+        discounted_price = price * (1 - self.discount_percent / 100)
+        return discounted_price
+
